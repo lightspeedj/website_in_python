df3639598a082537f44ade99e129ce9cc767ad77
lightspeedj <SamDevelop9#>
2018-04-02 15:51:23 +0200

    - commit of whole django project

---
+++ music/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import include, url
+from . import views
+
+app_name = 'music'
+
+urlpatterns = [
+    #/music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
+    #/music/<album.id>
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    # /music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2/update
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumCreate.as_view(), name='album-update'),
+
+    # /music/album/2/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumCreate.as_view(), name='album-delete'),
+
+]
